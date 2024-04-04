cde2e58648ba019797a3d2f692646a65bc86cc16
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-04-04 01:17:29 -0400

    working on modal submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,66 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+//modal form and local storage
+const formModal = $('#formModal')
+const taskContainer = $('#todo-cards')
 const addTaskBtn = $("#taskBtn")
 const closeModalBtn = $('#closeModal')
+const submitModalBtn = $('#saveTask')
 
-let titleInput = formModal['title']
-let dateInput = formModal['date']
-let descriptionInput = formModal['description']
+let titleInput = $('#task-title')
+let dateInput = $('#task-due-date')
+let descriptionInput = $('#task-description')
 const tasks = [];
 
-const addTask = (title, date, description) => {
+//basic modal functionality
+addTaskBtn.on('click', () => $('#formModal').modal('show'))
+closeModalBtn.on('click', () => $('#formModal').modal('hide'))
 
+//pushing new tasks to the array
+const addTask = (title, date, description) => {
+    tasks.push({
+        title,
+        date,
+        description,
+    });
+    return {title, date, description};
 }
 
+const createTaskCard = ({title, date, description}) => {
+    const taskDiv = document.createElement('div');
+    const taskName = document.createElement('h3');
+    const taskDate = document.createElement('p');
+    const taskDesc = document.createElement('p');
 
-addTaskBtn.on('click', () => $('#formModal').modal(show))
-closeModalBtn.on('click', () => $('#formModal').modal(hide))
+    taskName.innerText = "Task Title: " + title;
+    taskDate.innerText = "Task Due Date: " + date;
+    taskDesc.innerText = "Task Description: " + description;
+
+    taskDiv.append(taskName, taskDate, taskDesc);
+    taskContainer.append(taskDiv);
+}
+
+tasks.forEach(createTaskCard)
+
+//putting the form input on the card when form is submitted
+submitModalBtn.addEventListener('click', () => {
+    const newTask = addTask(
+        titleInput.value,
+        dateInput.value,
+        descriptionInput.value,
+    );
+    
+    createTaskCard(newTask)
+
+    //clear the input every time
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = ""
+})
+
+
 
 
 // Todo: create a function to generate a unique task id
@@ -24,9 +69,7 @@ function generateTaskId() {
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
 
-}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
