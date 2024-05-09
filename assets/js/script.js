ce8152bfa97b0fda3bb8f5444ce5d3aab1a1e91d
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-05-09 11:09:48 -0400

    organized some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
 //modal form and local storage
 const formModal = $('#formModal')
 const taskContainer = $('#todo-cards')
@@ -8,7 +12,7 @@ const submitModalBtn = $('#saveTask')
 let titleInput = $('#task-title')
 let dateInput = $('#task-due-date')
 let descriptionInput = $('#task-description')
-const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 //drag and drop
 const draggables = document.querySelectorAll('.task-card');
@@ -20,20 +24,37 @@ closeModalBtn.on('click', () => $('#formModal').modal('hide'));
 
 
 //pushing new tasks to the array
-const addTask = (title, date, description) => {
+const addTask = (title, date, description, id) => {
+    // tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    // const newID = tasks.length
     tasks.push({
         title,
         date,
         description,
+        id: id,
     });
 
     localStorage.setItem("tasks", JSON.stringify(tasks))
-
+    
     return {title, date, description};
 };
 
+const removeElement = (event) => {
+    const button = event.target;
+    button.parentElement.remove();
+    const id = button.getAttribute('data-task');
+    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.filter((task) => {
+        task.id !== id
+        console.log("task id", task.id)
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    console.log("id of task", id)
+    
+}
+
 //adding the card format
-const createTaskCard = ({title, date, description}) => {
+const createTaskCard = ({title, date, description, id}) => {
     const taskDiv = document.createElement('div');
     taskDiv.setAttribute('draggable', 'true');
     taskDiv.setAttribute('class', 'task-card')
@@ -44,6 +65,13 @@ const createTaskCard = ({title, date, description}) => {
     const taskDesc = document.createElement('p');
     const deleteBtn = document.createElement('button')
     deleteBtn.setAttribute('id', 'deleteBtn')
+
+    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const newID = tasks.length -1
+
+    deleteBtn.setAttribute('data-task', id)
+
+    deleteBtn.addEventListener('click', removeElement);
 
     taskName.innerText = "Task Title: " + title;
     taskDate.innerText = "Task Due Date: "
@@ -58,26 +86,17 @@ const createTaskCard = ({title, date, description}) => {
 tasks.forEach(createTaskCard);
 
 
-//delete btn
-const deleteBtns = document.getElementById('deleteBtn')
-
-deleteBtns.forEach((button) => {
-    button.addEventListener('click', () => { 
-        button.parentElement.remove();
-        localStorage.removeItem()
-    })
-})
-
-
 //putting the form input on the card when form is submitted
 submitModalBtn.on('click', (event) => {
     event.preventDefault();
-
+    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const id = tasks.length
     // console.log('the task was submitted')
     const newTask = addTask(
         titleInput.val(),
         dateInput.val(),
-        descriptionInput.val()
+        descriptionInput.val(),
+        id,
     );
     
     $('#formModal').modal('hide');
@@ -90,9 +109,10 @@ $(document).ready(function () {
     $('.task-card').draggable();
     $('.swim-droppable').droppable({
         accept: '.draggable',
-    })
-})
+    });
 
+    console.log("DOM Loaded");
+});
 
 //datepicker
 $(function(){
@@ -107,15 +127,17 @@ console.log(parsedUserDate)
 
 const dueDateColor = () => {
     draggables.forEach((draggable) => {
-        if (parsedUserDate.isAfter(dayjs())) {
-        draggable.setAttribute('class', 'over-due')
-    } else if (parsedUserDate.isSame(dayjs())) {
-        draggable.setAttribute('class','due-soon')
-    } else {
-        console.log('the task is not due soon')
-    };
-    })
-    
-}
+        const taskDateInput = draggable.querySelector('#taskDateInput').textContent;
+        const parsedUserDate = dayjs(taskDateInput);
+        console.log("parsedUserDate = " + parsedUserDate);
+        if (parsedUserDate.isBefore(dayjs(), 'day')) {
+            draggable.classList.add('over-due');
+        } else if (parsedUserDate.isSame(dayjs(), 'day') || parsedUserDate.isBefore(dayjs().add(2, 'day'), 'day')) {
+            draggable.classList.add('due-soon');
+        } else {
+            draggable.classList.remove('over-due', 'due-soon');
+        }
+    });
+};
 
 dueDateColor();
